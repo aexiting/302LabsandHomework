b40790beb0774fb47af488b4c3e4a5091c440bfb
Adam Pettway <adamttway@gmail.com>
2018-02-08 04:53:35 -0500

    Now doing Lab3

---
+++ Lab3/Lab3.java
@@ -0,0 +1,45 @@
+
+class Lab3 {
+
+    public void RunTestSuite() {
+    }
+
+    public void RunTimeSuite() {
+        System.out.printf("Timing mergesorts...%n");
+        System.out.printf("%15s %8s %8s %8s%n", "n", "Aloc.", "NoAloc.", "NoRec.");
+
+        for (int size = 1; size > 0 && size < 2000000000; size=size + (int)((size+1)*0.5)) {
+            MergeSortLab3 mergeSortLab3 = new MergeSortLab3();
+            int[] arrayToSort = new int[size];
+
+            System.out.printf("%15d", size);
+
+            long startTimeMillis = System.currentTimeMillis();
+            mergeSortLab3.RecursiveMergeSortWithAllocation(arrayToSort, 0, arrayToSort.length-1);
+            System.out.printf(" %8.2f", (System.currentTimeMillis() - startTimeMillis)/1.0); 
+
+            startTimeMillis = System.currentTimeMillis();
+            mergeSortLab3.RecursiveMergeSortWithoutAllocation(arrayToSort, 0, arrayToSort.length-1);
+            System.out.printf(" %8.2f", (System.currentTimeMillis() - startTimeMillis)/1.0); 
+
+            startTimeMillis = System.currentTimeMillis();
+            mergeSortLab3.NonRecursiveMergeSortWithoutAllocation(arrayToSort);
+            System.out.printf(" %8.2f", (System.currentTimeMillis() - startTimeMillis)/1.0); 
+
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+        Lab3 lab3 = new Lab3();
+        if (args[0].equals("--test")) {
+            lab3.RunTestSuite();
+        } else if (args[0].equals("--time")) {
+            lab3.RunTimeSuite();
+        } else {
+            System.out.println("Unrecognized command line argument: " + args[0]);
+        }
+    }
+};
---
+++ Lab3/MergeSortLab3.java
@@ -0,0 +1,21 @@
+
+class MergeSortLab3
+{
+    MergeSortLab3() {
+    }
+
+    void MergeWithAllocation(int[] integerArray, int startIndex, int midIndex, int endIndex) {
+    }
+
+    void MergeWithoutAllocation(int[] integerArray, int startIndex, int midIndex, int endIndex) {
+    }
+
+    void RecursiveMergeSortWithAllocation(int[] integerArray, int startIndex, int endIndex) {
+    }
+
+    void RecursiveMergeSortWithoutAllocation(int[] integerArray, int startIndex, int endIndex) {
+    }
+
+    void NonRecursiveMergeSortWithoutAllocation(int[] integerArray) {
+    }
+};
